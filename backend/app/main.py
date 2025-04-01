d0154699b9ba5d23cc59e535cae98a0ff38ca16f
Gupta, Harshit Hemant <hhgupta@iu.edu>
2025-04-01 10:35:21 -0400

    added models and db python file
    Loaded the data in temporary table called threat_data.db

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,12 @@
 from fastapi import FastAPI
+from .db import init_db
 
 app = FastAPI()
 
+@app.on_event("startup")
+def startup_event():
+    init_db()
+
 @app.get("/")
-def read_root():
-    return {"status": "Backend is running!"}
+def root():
+    return {"status": "Backend is running"}
